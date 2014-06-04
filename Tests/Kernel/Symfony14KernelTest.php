11ca72440a9eb6c388a84303cba4a32868ae085b
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2014-06-04 22:03:21 +0200

    Add tests for Symfony14Kernel

---
+++ Tests/Kernel/Symfony14KernelTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Kernel;
+
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTestCase;
+use Theodo\Evolution\Bundle\LegacyWrapperBundle\Kernel\Symfony14Kernel;
+
+/**
+ * Symfony14KernelTest
+ * 
+ * @author Benjamin Grandfond <benjaming@theodo.fr>
+ */
+class Symfony14KernelTest extends ProphecyTestCase
+{
+    public function testShouldBoot()
+    {
+        $classLoader = $this->prophesize('Theodo\Evolution\Bundle\LegacyWrapperBundle\Autoload\LegacyClassLoaderInterface');
+        $container   = $this->prophesize('Symfony\Component\DependencyInjection\ContainerInterface');
+
+        $classLoader->setKernel(Argument::type('Theodo\Evolution\Bundle\LegacyWrapperBundle\Kernel\Symfony14Kernel'))->shouldBeCalled();
+        $classLoader->isAutoloaded()->willReturn(false);
+        $classLoader->autoload()->shouldBeCalled();
+
+        $kernel = new Symfony14Kernel(__DIR__.'/fixtures/symfony14', $classLoader->reveal());
+        $kernel->boot($container->reveal());
+
+        $this->assertTrue($kernel->isBooted());
+    }
+}
+ 
---
+++ Tests/Kernel/fixtures/symfony14/config/ProjectConfiguration.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class ProjectConfiguration
+{
+    public static function getApplicationConfiguration($application, $environment, $debug)
+    {
+
+    }
+}
+
+class sfContext
+{
+    public static function createInstance($configuration)
+    {
+
+    }
+}
